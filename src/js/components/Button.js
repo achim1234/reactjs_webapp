067287f71eceeba8d7bb9fc4bea1e50de4323b88
Achim <astrohm@gmx.net>
2017-04-10 12:04:55 +0200

    made some changes

--- src/js/components/Button.js
+++ src/js/components/Button.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import RevolverItem from "./RevolverItem";
+import ButtonStyles from "./ButtonStyles";
 
 export default class Button extends React.Component {
 	constructor(props) {
@@ -28,23 +29,27 @@ export default class Button extends React.Component {
 	
 	render() {
 		const revItem = {			
-			backgroundColor: '#EF5350',
-			color: 'white',
+			backgroundColor: ButtonStyles.colorButton,
+			color: ButtonStyles.colorWhite,
 			border: 'none',
 			margin: 5,
-		}
+			marginTop: 30
+		};
+
+		const mainCentered = {
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center'
+		};
 
 		return (
 			<div id="revolverButtonBox">
-				
-				<div id="" class="container">
-					<button id="btnRev" style={revItem} onTouchStart={this.buttonPressed} onTouchCancel={this.touchEnded} onTouchEnd={this.touchEnded}>Hallo</button>
-				</div>
+
 
 				<div id="revolverItems" className="revBtnTwo">
 
 				{this.state.showComponent ? 
-					<RevolverItem styleProp={revItem} /> : 
+					<RevolverItem styleProp={revItem} className="fixed-top" /> : 
 					null
 				}
 				{this.state.showComponent ? 
@@ -52,7 +57,28 @@ export default class Button extends React.Component {
 					null
 				}
 				{this.state.showComponent ? 
+					<RevolverItem styleProp={revItem} className="fixed-bottom" /> : 
+					null
+				}
+				</div>
+
+				
+				<div style={mainCentered}>
+					<button id="btnRev" style={revItem} onTouchStart={this.buttonPressed} onTouchCancel={this.touchEnded} onTouchEnd={this.touchEnded}>Hallo</button>
+				</div>
+
+				<div id="revolverItems2" className="revBtnTwo">
+
+				{this.state.showComponent ? 
+					<RevolverItem styleProp={revItem} className="fixed-top" /> : 
+					null
+				}
+				{this.state.showComponent ? 
 					<RevolverItem styleProp={revItem} /> : 
+					null
+				}
+				{this.state.showComponent ? 
+					<RevolverItem styleProp={revItem} className="fixed-bottom" /> : 
 					null
 				}
 				</div>
